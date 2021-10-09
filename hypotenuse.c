067de782878ea0417f18b23961c711142eb9f66d
UneBaguette <thomasiger42@gmail.com>
2021-10-09 19:10:32 +0200

    Update hypotenuse.c

--- hypotenuse.c
+++ hypotenuse.c
@@ -3,15 +3,19 @@
 
 int main()
 {
+    // déclaration de mes variables
     int ab, bc;
     double r;
+    // le programme débute ici
     printf("Programme pour calculer l'hypoténuse, soit ABC un triangle rectangle en B\n");
     printf("Quelle est la longueur de AB? (en cm) : ");
-    scanf("%i", &ab);
+    scanf("%i", &ab); // enregistre dans ab le nombre ou le chiffre que l'on tape
     printf("Quelle est la longueur de BC? (en cm) : ");
-    scanf("%i", &bc);
+    scanf("%i", &bc); // enregistre dans bc le nombre ou le chiffre que l'on tape
+    // le calcul est fait ici
     ab = pow(ab, 2);
     bc = pow(bc, 2);
     r = ab + bc;
+    // le résulat du calcul
     printf("La longueur de l'hypoténuse est de %.2f cm\n", sqrt(r));
 }
